2eb47fdc06a00a28f99931f072336b25c1e86c97
rlaxogh76 <btm.email2769@gmail.com>
2025-11-13 14:48:16 +0900

    fix: AuthContext.tsx 경로 수정

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { useAuth } from "../../auth/AuthContext.tsx";
+import { useAuth } from "../../auth/AuthContext";
 
 const Login = () => {
   const [id, setId] = useState("");
