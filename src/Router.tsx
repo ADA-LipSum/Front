da89818c73f5ab7f072ad60d913023c5598aff26
rlaxogh76 <btm.email2769@gmail.com>
2026-02-17 18:42:15 +0900

    feat: 라우터 구성 변경 및 메인 레이아웃 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home/Home';
-import { Community } from './pages/Community/Contact';
-import { Exchange } from './pages/Exchange/Exchange';
-import { Event } from './pages/Event/Event';
-import { Contact } from './pages/Contact/Contact';
+import { Main } from '@/pages/Main/Main';
+import { Community } from '@/pages/Community/Community';
+import { Exchange } from '@/pages/Exchange/Exchange';
+import { Event } from '@/pages/Event/Event';
+import { Contact } from '@/pages/Contact/Contact';
+import { Login } from '@/pages/Auth/Login';
+import { SocialLogin } from '@/pages/Auth/SocialLogin';
+import MainLayout from '@/components/layout/MainLayout';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/community" element={<Community />} />
-      <Route path="/exchange" element={<Exchange />} />
-      <Route path="/event" element={<Event />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/exchange" element={<Exchange />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+
+      {/* 로그인 페이지 */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/social-login" element={<SocialLogin />} />
     </Routes>
   );
 };
