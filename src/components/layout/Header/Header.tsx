9714a42ce5797e5c2bd857c9d12fe0b2fa95afed
rlaxogh76 <btm.email2769@gmail.com>
2026-02-19 15:56:36 +0900

    feat: 로그인 상태 prop 제거 및 useSelector로 상태 관리 통합

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,22 +1,18 @@
 import { Link } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 import LipSum_Logo_Black from '@/assets/LipSum-logo-black.svg';
 import { Bell } from 'lucide-react';
+import type { RootState } from '@/store/store';
 
-type HeaderProps = {
-  // 로그인 상태를 나타내는 prop (예: isLoggedIn)
-  isLoggedIn: boolean;
-};
+const Header = () => {
+  const { isLoggedIn, user } = useSelector((state: RootState) => state.auth);
 
-const Header = ({ isLoggedIn }: HeaderProps) => {
   return (
     <div className="w-full h-20 bg-[#F5F5F5] flex items-center pl-18.25 border-b border-[#9C9C9C]">
-      {/* 로고 */}
       <Link to="/">
         <img src={LipSum_Logo_Black} className="w-40" alt="logo" />
       </Link>
 
-      {/* 메뉴 영역 */}
       <div className="flex items-center ml-20 gap-10 text-black font-semibold">
         <Link to="/community">커뮤니티</Link>
         <Link to="/exchange">거래소</Link>
@@ -29,8 +25,9 @@ const Header = ({ isLoggedIn }: HeaderProps) => {
           <>
             <Bell className="w-6 h-6 cursor-pointer hover:text-blue-600" />
             <img
-              src="https://api.dicebear.com/9.x/identicon/svg?seed=a9739cf1d10211f0bd93862ccfb0399f"
+              src={user?.profileImage}
               className="w-12 h-12 rounded-full ml-2 border border-gray-300 hover:cursor-pointer"
+              alt="profile"
             />
           </>
         ) : (
