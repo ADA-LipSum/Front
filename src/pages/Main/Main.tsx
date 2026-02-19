0e4eb4fd607d129218cecdcd4a71f70f88c5e609
rlaxogh76 <btm.email2769@gmail.com>
2026-02-19 15:57:23 +0900

    feat: 메인 페이지(/)에 임시 로그인 여부 표시 및 로그아웃 버튼 추가

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,3 +1,35 @@
+import type { AppDispatch, RootState } from '@/store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutAsync } from '@/features/auth/authSlice';
+
 export const Main = () => {
-  return <div>Main</div>;
+  const { isLoggedIn } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleLogout = async () => {
+    try {
+      await dispatch(logoutAsync()).unwrap();
+      alert('로그아웃 성공!');
+    } catch (err) {
+      alert('로그아웃 실패');
+    }
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <p>로그인 상태입니다.</p>
+          <button
+            onClick={handleLogout}
+            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
+          >
+            로그아웃
+          </button>
+        </div>
+      ) : (
+        <div>로그인 상태가 아닙니다.</div>
+      )}
+    </div>
+  );
 };
