bbe6974f7d2a7cea5201c76f08c7eb8ee36ef0a5
rlaxogh76 <btm.email2769@gmail.com>
2026-03-07 21:29:34 +0900

    feat: 사용자 코인 배너 컴포넌트 추가

---
+++ src/components/exchange/UserInfo.tsx
@@ -0,0 +1,12 @@
+export const UserCoinBanner = () => {
+  return (
+    <div className="w-full h-20">
+      <div className="ml-48 mt-6 font-bold">
+        <div className="flex gap-27">
+          <p className="">코인</p>
+          <p className="">포인트</p>
+        </div>
+      </div>
+    </div>
+  );
+};
