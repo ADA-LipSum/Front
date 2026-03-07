a1806763af7a6c9b36c2ca9608af8a86c7d43604
rlaxogh76 <btm.email2769@gmail.com>
2026-03-07 21:29:28 +0900

    feat: 거래소 배너 컴포넌트 추가

---
+++ src/components/exchange/ExchangeBanner.tsx
@@ -0,0 +1,7 @@
+export const ExchangeBanner = () => {
+  return (
+    <div className="w-full h-32.5 bg-[#7CBEBF]">
+      <h1 className="text-4xl font-bold ml-48 pt-10.75 text-white">거래소</h1>
+    </div>
+  );
+};
