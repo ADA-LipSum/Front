f6dcb497276c66cdbd1fb54ad9b79c7d9d266753
rlaxogh76 <btm.email2769@gmail.com>
2026-03-17 10:25:48 +0900

    feat: 사용자 정보 구조 변경 및 로그인 상태 업데이트 로직 개선
    - 로그인한 유저의 uuid를 가져올 수 있도록 구현
    - 인터페이스 타입 이름 변경 및 추가

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -5,9 +5,10 @@ import { login as loginApi } from '@/api/auth';
 interface User {
   uuid: string;
   role: string;
-  userRealname: string;
-  userNickname: string;
+  realname: string;
+  nickname: string;
   profileImage: string;
+  profileBanner: string;
   isFirstLogin: boolean;
 }
 
@@ -31,31 +32,42 @@ export const login = createAsyncThunk(
 
     const token = res.data.accessToken;
 
-    // localStorage 저장
-    localStorage.setItem('accessToken', token);
+    localStorage.setItem('accessToken', token); // 로컬 스토리지에 토큰 저장
 
-    // axios 기본 헤더 세팅
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`; // axios 기본 헤더에 토큰 설정
 
-    // 사용자 정보 조회
-    const me = await axios.get('auth/status');
+    const me = await axios.get('auth/status'); // 로그인 후 사용자 정보 가져오기
 
-    return me.data.data;
+    const { uuid, user } = me.data.data; // 응답에서 uuid와 user 정보 추출
+
+    return {
+      uuid,
+      ...user,
+    };
   },
 );
 
 // 새로고침 시 로그인 유지
 export const checkLogin = createAsyncThunk(
   'api/auth/checkLogin',
+
+  // 로컬 스토리지에서 토큰 가져오기
   async (_, { rejectWithValue }) => {
     const token = localStorage.getItem('accessToken');
 
-    if (!token) return rejectWithValue(null);
+    if (!token) return rejectWithValue(null); // 토큰이 없으면 로그인 상태가 아님
 
     try {
       axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
       const res = await axios.get('auth/status');
-      return res.data.data;
+
+      const { uuid, user } = res.data.data;
+
+      return {
+        uuid,
+        ...user,
+      };
     } catch (err) {
       localStorage.removeItem('accessToken');
       return rejectWithValue(null);
@@ -75,21 +87,25 @@ const authSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(login.fulfilled, (state, action) => {
+        // 로그인 성공 시 상태 업데이트
         state.isLoggedIn = true;
         state.loading = false;
         state.user = action.payload;
       })
       .addCase(checkLogin.fulfilled, (state, action) => {
+        // 로그인 상태 유지 성공 시 상태 업데이트
         state.isLoggedIn = true;
         state.loading = false;
         state.user = action.payload;
       })
       .addCase(checkLogin.rejected, (state) => {
+        // 로그인 상태 유지 실패 시 상태 업데이트
         state.isLoggedIn = false;
         state.loading = false;
         state.user = null;
       })
       .addCase(logoutAsync.fulfilled, (state) => {
+        // 로그아웃 성공 시 상태 업데이트
         state.isLoggedIn = false;
         state.user = null;
       });
