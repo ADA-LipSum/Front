bf08095f3b5c43aabb8ab1ee91443f6078bd0e9d
rlaxogh76 <btm.email2769@gmail.com>
2026-03-17 19:20:33 +0900

    feat: 프로필 컴포넌트에서 사용자 정보를 비동기로 가져오도록 수정

--- src/components/profile/Intro.tsx
+++ src/components/profile/Intro.tsx
@@ -1,7 +1,28 @@
-const Intro = ({ intro }: { intro: string }) => {
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import type { RootState } from '@/store/store';
+import { getProfile } from '@/api/profile';
+
+const Intro = () => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [intro, setIntro] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      if (!user?.uuid) return;
+
+      const res = await getProfile(user.uuid);
+
+      // 핵심 부분
+      setIntro(res.data.intro);
+    };
+
+    fetchProfile();
+  }, [user]);
+
   return (
-    <div className="w-1 p-2 bg-gray-200 rounded-lg ml-80 -mt-25">
-      <p className="text-gray-600">{intro}</p>
+    <div className="">
+      <p className="text-gray-600">{user?.intro}</p>
     </div>
   );
 };
