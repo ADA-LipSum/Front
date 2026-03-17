b200f2c3c9a8a030382fe36bb70d8b80abb5a933
rlaxogh76 <btm.email2769@gmail.com>
2026-03-17 12:13:41 +0900

    fix: 프로필 컴포넌트에서 사용자 정보를 가져오도록 수정

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,23 @@
-// import Intro from '@/components/profile/Intro';
+import Intro from '@/components/profile/Intro';
 import ProfileBanner from '@/components/profile/ProfileBanner';
 import ProfileImage from '@/components/profile/ProfileImage';
 import UserNameText from '@/components/profile/UserNameText';
 
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from '@/store/store';
+import { fetchProfile } from '@/features/auth/profileSlice';
+
 const Profile = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    if (user?.uuid) {
+      dispatch(fetchProfile(user.uuid));
+    }
+  }, [user, dispatch]);
+
   return (
     <>
       <ProfileBanner />
@@ -12,6 +26,7 @@ const Profile = () => {
           <div>
             <ProfileImage />
             <UserNameText />
+            <Intro />
           </div>
         </div>
       </div>
