3e9985ed97a42a9e9359eb9a85e341408dfb09c4
rlaxogh76 <btm.email2769@gmail.com>
2026-03-25 13:42:54 +0900

    feat: 프로필 링크를 사용자 ID 기반으로 수정하여 프로필 페이지 접근 개선

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -38,7 +38,7 @@ const Header = () => {
               <div className="w-10 h-10 border-[#ffffff] bg-gray-100 border rounded-sm flex items-center justify-center relative inset-shadow-xs hover:cursor-pointer group">
                 <Bell className="w-5 h-5 transition-colors duration-200 group-hover:text-blue-600" />
               </div>
-              <Link to="/profile">
+              <Link to={`/profile/${user?.customId}`}>
                 <img
                   src={user?.profileImage}
                   className="w-12 h-12 rounded-full ml-2 border border-gray-300 hover:cursor-pointer"
