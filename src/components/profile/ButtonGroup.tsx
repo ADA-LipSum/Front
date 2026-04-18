1b1d859d30eb00b231f3e3f615e7c6225063eb23
rlaxogh76 <btm.email2769@gmail.com>
2026-04-18 23:22:11 +0900

    feat: ButtonGroup 컴포넌트에 props 추가 및 수정

--- src/components/profile/ButtonGroup.tsx
+++ src/components/profile/ButtonGroup.tsx
@@ -1,11 +1,18 @@
 import { EditButton } from './EditButton';
 import { InventoryButton } from './InventoryButton';
 
-export const ButtonGroup = () => {
+interface ButtonGroupProps {
+  isEditing: boolean;
+  onEdit: () => void;
+  onSave: () => Promise<void>;
+  onCancel: () => void;
+}
+
+export const ButtonGroup = ({ isEditing, onEdit, onSave, onCancel }: ButtonGroupProps) => {
   return (
     <div className="absolute right-4 top-110 flex gap-5 *:static">
-      <InventoryButton />
-      <EditButton />
+      {!isEditing && <InventoryButton />}
+      <EditButton isEditing={isEditing} onEdit={onEdit} onSave={onSave} onCancel={onCancel} />
     </div>
   );
 };
