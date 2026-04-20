ca2daadaf813338c3151705e45ef74fdadde1c76
rlaxogh76 <btm.email2769@gmail.com>
2026-04-20 19:37:08 +0900

    feat: storybook 초기화

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
@@ -5,54 +8,44 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import unusedImports from 'eslint-plugin-unused-imports';
 import prettier from 'eslint-config-prettier';
 
-export default [
-  {
-    ignores: ['dist', 'build', '.next', 'node_modules'],
+export default [{
+  ignores: ['dist', 'build', '.next', 'node_modules'],
+}, js.configs.recommended, ...tseslint.configs.recommended, {
+  files: ['**/*.{ts,tsx,js,jsx}'],
+  plugins: {
+    react,
+    'react-hooks': reactHooks,
+    'unused-imports': unusedImports,
   },
-
-  js.configs.recommended,
-
-  ...tseslint.configs.recommended,
-
-  {
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'unused-imports': unusedImports,
-    },
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      /* React */
-      'react/react-in-jsx-scope': 'off', // React 17+
-      'react/jsx-uses-react': 'off',
-
-      /* Hooks */
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-
-      /* TypeScript */
-      '@typescript-eslint/no-unused-vars': 'off',
-
-      /* Clean Code */
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-      ],
-
-      /* Console */
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
+  rules: {
+    /* React */
+    'react/react-in-jsx-scope': 'off', // React 17+
+    'react/jsx-uses-react': 'off',
 
-  prettier,
-];
+    /* Hooks */
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    /* TypeScript */
+    '@typescript-eslint/no-unused-vars': 'off',
+
+    /* Clean Code */
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+
+    /* Console */
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+}, prettier, ...storybook.configs["flat/recommended"]];
