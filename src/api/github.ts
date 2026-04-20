29671e7ccfd0159ace4e5195882f938368a952a1
rlaxogh76 <btm.email2769@gmail.com>
2026-04-20 16:14:58 +0900

    feat: 깃허브 API 연동 기능 추가

---
+++ src/api/github.ts
@@ -0,0 +1,30 @@
+import axios from './axios';
+
+// 깃허브 연동 정보 조회
+export const getUserGitHubInfo = async () => {
+  const response = await axios.get(`api/auth/github/status`);
+  return response.data.data;
+};
+
+// 깃허브 연동 시작
+export const getGitHubOAuthUrl = async () => {
+  const response = await axios.get(`api/auth/github/link`);
+  return response.data.data;
+};
+
+// 깃허브 연동 해제
+export const unlinkGitHub = async () => {
+  await axios.delete(`api/auth/github/unlink`);
+};
+
+// 깃허브 로그인
+export const getGitHubLogin = async () => {
+  const response = await axios.get(`api/auth/github/login`);
+  return response.data.data;
+};
+
+// 깃허브 잔디 조회
+export const getUserGitHubContributions = async (year: number) => {
+  const response = await axios.get(`api/auth/github/contributions?year=${year}`);
+  return response.data.data;
+};
