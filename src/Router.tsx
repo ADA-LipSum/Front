ebe1cffdbde47e481bc68a3d1d902b4254e9b5a3
rlaxogh76 <btm.email2769@gmail.com>
2026-04-28 10:55:47 +0900

    feat: 로그인 상태 확인 로직 개선 - useDispatch 대신 useAuthStore 사용 및 불필요한 코드 제거

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,8 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from '@/store/store';
+import { useAuthStore } from '@/store/authStore';
 
 import { Main } from '@/pages/Main/Main';
 import { Community } from '@/pages/Community/Community';
@@ -11,12 +10,10 @@ import { CommunityLayout } from '@/pages/Community/CommunityLayout';
 import { CommunityPostDetail } from '@/pages/Community/CommunityPostDetail';
 import { CommunityWrite } from '@/pages/Community/CommunityWrite';
 import { Exchange } from '@/pages/Exchange/Exchange';
-import { Event } from '@/pages/Event/Event';
 import { Contact } from '@/pages/Contact/Contact';
 import { Login } from '@/pages/Auth/Login';
 import { SocialLogin } from '@/pages/Auth/SocialLogin';
 import MainLayout from '@/components/layout/MainLayout';
-import { checkLogin } from './features/auth/authSlice';
 import Proifle from './pages/Profile/Profile';
 import UserNotFound from './pages/NotFound/UserNotFound';
 import { StudyGroup } from './pages/StudyGroup/StudyGroup';
@@ -24,11 +21,11 @@ import { Announcement } from './pages/Announcement/Contact';
 import { Settings } from './pages/Setting/Settings';
 
 const Router = () => {
-  const dispatch = useDispatch<AppDispatch>();
+  const checkLogin = useAuthStore((state) => state.checkLogin);
 
   useEffect(() => {
-    dispatch(checkLogin()); // 새로고침 시 로그인 상태 확인
-  }, [dispatch]);
+    checkLogin(); // 새로고침 시 로그인 상태 확인
+  }, [checkLogin]);
 
   return (
     <Routes>
@@ -40,7 +37,6 @@ const Router = () => {
           <Route path=":postId" element={<CommunityPostDetail />} />
         </Route>
         <Route path="/exchange" element={<Exchange />} />
-        <Route path="/event" element={<Event />} />
         <Route path="/study-group" element={<StudyGroup />} />
         <Route path="/announcement" element={<Announcement />} />
         <Route path="/contact" element={<Contact />} />
