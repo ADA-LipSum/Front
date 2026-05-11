5173c7b83bef5e6dc0c400043918191d3f83d79f
rlaxogh76 <btm.email2769@gmail.com>
2026-05-11 11:21:57 +0900

    feat: 파일 삭제

--- src/components/tags/H1.tsx
+++
@@ -1,5 +0,0 @@
-const H1 = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-  return <h1 className={`text-3xl font-bold ${className || ''}`}>{children}</h1>;
-};
-
-export default H1;
