faf48deb83f5406d9ac382bf27aa35d59361ea5d
rlaxogh76 <btm.email2769@gmail.com>
2026-05-14 16:54:49 +0900

    feat: ProfileDropdown 컴포넌트 스타일 및 구조 개선

--- src/components/Page/profile/ProfileDropdown.tsx
+++ src/components/Page/profile/ProfileDropdown.tsx
@@ -1,24 +1,46 @@
+import { Link } from 'react-router-dom';
+
+import { UserCircle, CreditCard, Settings, LogOut } from 'lucide-react';
+
 export default function ProfileDropdown() {
   return (
-    <div className="justify-center items-center mt-2 w-48 bg-white rounded-md shadow-md border-2 border-gray-100 py-1 z-20">
-      <p className="block px-4 py-2 text-md text-black">내 프로필</p>
-      <hr className="border-gray-200" />
-      {/* 프로필 */}
-      <section className="">
-        <a href="/profile/edit" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-          프로필
-        </a>
-      </section>
-      {/* 설정 */}
-      <section className="">
-        <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">설정</a>
-      </section>
-      {/* 로그아웃 */}
-      <section className="">
-        <a href="/logout" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-          로그아웃
-        </a>
-      </section>
+    <div className="w-52 bg-white border border-gray-200 rounded-md shadow-lg p-1.5">
+      <div className="flex flex-col">
+        <p className="px-3 py-2 text-base font-semibold">내 계정</p>
+
+        <hr className="my-1 border-gray-300" />
+
+        <Link
+          to="/mypage"
+          className="px-3 py-2 rounded-lg hover:bg-gray-100 transition flex items-center justify-between text-sm"
+        >
+          <span>프로필</span>
+          <UserCircle size={17} />
+        </Link>
+
+        <Link
+          to="/payments"
+          className="px-3 py-2 rounded-lg hover:bg-gray-100 transition flex items-center justify-between text-sm"
+        >
+          <span>결제 내역</span>
+          <CreditCard size={17} />
+        </Link>
+
+        <Link
+          to="/settings"
+          className="px-3 py-2 rounded-lg hover:bg-gray-100 transition flex items-center justify-between text-sm"
+        >
+          <span>계정 관리</span>
+          <Settings size={17} />
+        </Link>
+
+        <hr className="my-1 border-gray-300" />
+
+        <button className="text-left px-3 py-2 rounded-lg hover:bg-red-50 text-red-500 transition flex items-center justify-between text-sm">
+          <span>로그아웃</span>
+          <LogOut size={17} />
+        </button>
+      </div>
     </div>
   );
 }
