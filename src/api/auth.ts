a90c406ccb154b81b5fc048f90b687408fe5c4ec
rlaxogh76 <btm.email2769@gmail.com>
2026-05-14 10:27:36 +0900

    feat: 로그인 API 및 인증 상태 조회 함수 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,10 +1,17 @@
 import axios from './axios';
 
+// 로그인 API
 export const login = async (id: string, password: string) => {
   const response = await axios.post('api/auth/login', {
     id,
     password,
   });
 
+  return response.data;
+};
+
+// 인증 상태 조회
+export const checkAuth = async () => {
+  const response = await axios.get('api/auth/status');
   return response.data;
 };
