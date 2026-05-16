db1b3e8109797598a9252e0d2a0a6a067d316fcb
rlaxogh76 <btm.email2769@gmail.com>
2026-05-16 23:47:01 +0900

    feat: Settings 페이지 레이아웃 구현 및 라우팅 설정 추가

--- src/pages/Setting/Settings.tsx
+++ src/pages/Setting/Settings.tsx
@@ -1,6 +1,38 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { SettingsSidebar } from './SettingsSidebar';
+import { ProfileSettings } from './ProfileSettings';
+import { ConnectedAccountSettings } from './ConnectedAccountSettings';
+import { DisplaySettings } from './DisplaySettings';
+import { NotificationSettings } from './NotificationSettings';
+
 export const Settings = () => {
   return (
-    // 왼쪽 섹션
-    <></>
+    <div className="min-h-screen bg-[#f7f8fa]">
+      <div className="max-w-350 mx-auto flex px-10 py-10 gap-16">
+        {/* 왼쪽 사이드바 */}
+        <SettingsSidebar />
+
+        {/* 수직 구분선 */}
+        <div
+          className="
+              w-px
+              bg-gray-300
+              self-stretch
+            "
+        />
+
+        {/* 오른쪽 컨텐츠 */}
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Navigate to="/settings/profile" replace />} />
+
+            <Route path="/profile" element={<ProfileSettings />} />
+            <Route path="/account" element={<ConnectedAccountSettings />} />
+            <Route path="/display" element={<DisplaySettings />} />
+            <Route path="/notifications" element={<NotificationSettings />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 };
