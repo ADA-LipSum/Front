4a5423bce01c57380a0db777ea026b33a158aae4
rlaxogh76 <btm.email2769@gmail.com>
2026-05-18 14:25:32 +0900

    feat: 프로필 드롭다운에서 인벤토리 및 설정 링크 수정, 아바타 추가

--- src/components/Page/profile/ProfileDropdown.tsx
+++ src/components/Page/profile/ProfileDropdown.tsx
@@ -1,7 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom';
 
-import { UserCircle, Settings, LogOut, Bookmark, Mail } from 'lucide-react';
+import { UserCircle, Settings, LogOut, Bookmark, Box, Mailbox } from 'lucide-react';
 import { useAuthStore } from '@/store/authStore';
+import Avatar from '@/components/global/Avatar';
 
 export default function ProfileDropdown() {
   const navigate = useNavigate();
@@ -17,7 +18,13 @@ export default function ProfileDropdown() {
   return (
     <div className="w-52 bg-white border border-gray-200 rounded-md shadow-lg p-1.5">
       <div className="flex flex-col">
-        <p className="px-3 py-2 text-base font-semibold">내 계정</p>
+        <div className="flex items-center justify-between px-3 py-2">
+          <p className="text-base font-semibold">내 계정</p>
+          <Avatar
+            name={'프로필 이미지'}
+            src={useAuthStore.getState().user?.profileImage || undefined}
+          />
+        </div>
 
         <hr className="my-1 border-gray-300" />
 
@@ -30,11 +37,11 @@ export default function ProfileDropdown() {
         </Link>
 
         <Link
-          to="/settings"
+          to="/inventory"
           className="px-3 py-2 rounded-lg hover:bg-gray-100 transition flex items-center justify-between text-sm"
         >
-          <span>설정</span>
-          <Settings size={17} />
+          <span>인벤토리</span>
+          <Box size={17} />
         </Link>
 
         <Link
@@ -46,11 +53,19 @@ export default function ProfileDropdown() {
         </Link>
 
         <Link
+          to="/settings"
+          className="px-3 py-2 rounded-lg hover:bg-gray-100 transition flex items-center justify-between text-sm"
+        >
+          <span>설정</span>
+          <Settings size={17} />
+        </Link>
+
+        <Link
           to="/contact"
           className="px-3 py-2 rounded-lg hover:bg-gray-100 transition flex items-center justify-between text-sm"
         >
           <span>문의하기</span>
-          <Mail size={17} />
+          <Mailbox size={17} />
         </Link>
 
         <hr className="my-1 border-gray-300" />
