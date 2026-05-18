c8b1f6228de56c498b9f3a7253199d27569b5eb7
rlaxogh76 <btm.email2769@gmail.com>
2026-05-18 16:09:31 +0900

    feat: 커뮤니티 게시글 작성 페이지 컴포넌트 추가

---
+++ src/pages/Community/CommunityPostCreate.tsx
@@ -0,0 +1,7 @@
+export const CommunityPostCreate = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">커뮤니티 게시글 작성 페이지</h1>
+    </div>
+  );
+};
